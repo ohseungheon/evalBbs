87f6ea3955a4710389f2a773c4fd800145ff9867
osh <oshble5151@gmail.com>
2024-04-26 15:05:12 +0900

    커밋2-목록보기기능 구현

--- main/java/com/green/evalBbs/controller/EvalBbsController.java
+++ main/java/com/green/evalBbs/controller/EvalBbsController.java
@@ -1,14 +1,31 @@
 package com.green.evalBbs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.green.evalBbs.dao.IEvalBbsDao;
+
 @Controller
 public class EvalBbsController {
+	
+	@Autowired
+	private IEvalBbsDao dao;
+	
 	@RequestMapping("/")
 	public String root() {
 		return "index";
 	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("list",dao.getList() );
+		
+		return "list";
+	}
+	
 
 }
